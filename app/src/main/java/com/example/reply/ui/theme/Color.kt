4a6d64ec19f1700804a01ec6f1ebf8069c43fa6d
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-04-10 22:35:32 -0400

    Add .

--- app/src/main/java/com/example/reply/ui/theme/Color.kt
+++ app/src/main/java/com/example/reply/ui/theme/Color.kt
@@ -21,10 +21,10 @@ import androidx.compose.ui.graphics.Color
 // Generate them via theme builder
 // https://material-foundation.github.io/material-theme-builder/#/custom
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEBAEBF)
+val Purple80 = Color(0xFFC8B1FD)
+val PurpleGrey80 = Color(0xFFC8BADF)
+val Pink80 = Color(0xFFEEA5B9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF6147A8)
+val PurpleGrey40 = Color(0xFF5E5374)
+val Pink40 = Color(0xFF80495B)
