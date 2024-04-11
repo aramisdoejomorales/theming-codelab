c5a38a3dfa8e3e1830bac3d0208ab3cb5af2beef
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-04-11 00:24:10 -0400

    Add .

--- app/src/main/java/com/example/reply/ui/theme/Theme.kt
+++ app/src/main/java/com/example/reply/ui/theme/Theme.kt
@@ -16,10 +16,16 @@
 
 package com.example.reply.ui.theme
 
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 // Material 3 color schemes
 private val DarkColorScheme = darkColorScheme(
@@ -32,20 +38,27 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
 )
 
 @Composable
 fun ReplyTheme(
-    darkTheme: Boolean = false,
+    useDarkTheme: Boolean =  isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val replyColorScheme = when {
-        darkTheme -> DarkColorScheme
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     MaterialTheme(
-        colorScheme = replyColorScheme,
+        colorScheme = colors,
+        typography = typography,
+        shapes = shapes,
         content = content
     )
 }
